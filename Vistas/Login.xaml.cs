3826ce8406f76eb2747af1883fffc38f403c2268
Santiago Tintilay <santiago24080218@gmail.com>
2022-08-28 00:57:18 -0300

    second commit

---
+++ Vistas/Login.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        // para el hardcode
+        public struct usuario
+        {
+            public string user;
+            public string pass;
+        }
+
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            usuario admin, vendedor;
+            admin.user = "santy";
+            admin.pass = "santy";
+            vendedor.user = "mayko";
+            vendedor.pass = "mayko";
+
+            if(txtUser.Text == admin.user && txtPass.Text == admin.pass || txtUser.Text == vendedor.user && txtPass.Text == vendedor.pass)
+            {
+                MessageBox.Show("Bienvenido "+txtUser.Text);
+                Main main = new Main();
+                if (txtUser.Text == admin.user)
+                    main.userLog.tipoUsuer = "admin";   
+                else
+                    main.userLog.tipoUsuer = "vendedor";
+
+                main.validar();
+                main.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("usuario y/o contraseña incorrectos");
+            }
+
+
+            
+        }
+    }
+}
---
+++ Vistas/Main.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        public struct usuarioLog
+        { 
+            public string tipoUsuer;
+        }
+        public usuarioLog userLog;
+
+        public Window proveedores;
+        public Window clientes;
+        public Window actual;
+
+        public Main()
+        {
+            InitializeComponent();
+            
+        }
+        public void validar()
+        {
+           if (userLog.tipoUsuer == "vendedor")
+               vendedorItem.Visibility = Visibility.Collapsed;
+        }
+
+        //botones para cambiar de gestion 
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (actual != null) actual.Close();
+            proveedores = new Proveedores();
+            actual = proveedores;
+            actual.Show();
+        }
+
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (actual != null) actual.Close();
+            clientes = new Clientes();
+            actual = clientes;
+            actual.Show();
+        }
+
+
+    }
+}
