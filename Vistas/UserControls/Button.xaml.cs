9e48e77d3781a08f952c754465fcb6e45c68e57f
Samas-PC\Samas <samueleliasparedes.10@gmail.com>
2022-09-05 20:30:50 -0300

    Samuel Paredes Agregue los estilos, plantillas y triggers en archivos separados, solo hay que aplicarlos Agregue la carpeta de UserControls donde estan los controles de usuairo del login, solo hay que hacelros andar Queda en manos de ustedes

---
+++ Vistas/UserControls/Button.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Vistas.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Button.xaml
+    /// </summary>
+    public partial class Button : UserControl
+    {
+        public Button()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+    }
+}
